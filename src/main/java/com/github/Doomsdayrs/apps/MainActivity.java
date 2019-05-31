fc014053b6cabbb84b41a75102b10e146e0ecc10
doomsdayrs <rahim13657>
2019-05-31 06:59:15 -0400

    And GNU

--- src/main/java/com/github/Doomsdayrs/apps/MainActivity.java
+++ src/main/java/com/github/Doomsdayrs/apps/MainActivity.java
@@ -12,6 +12,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.SearchView;
 
+/**
+ * This file is part of novelreader-core.
+ * novelreader-core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with novelreader-core.  If not, see <https://www.gnu.org/licenses/>.
+ * ====================================================================
+ * novelreader-core
+ * 31 / May / 2019
+ *
+ * @author github.com/doomsdayrs
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
