4fe91ae406af83ef3eb5c79fa6fa079c83714083
doomsdayrs <rahim13657>
2019-05-31 21:14:38 -0400

    Redesigned app

--- app/src/androidTest/java/com/github/Doomsdayrs/apps/shosetsu/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/Doomsdayrs/apps/shosetsu/ExampleInstrumentedTest.java
@@ -1,9 +1,8 @@
 package com.github.Doomsdayrs.apps.shosetsu;
 
 import android.content.Context;
-
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
