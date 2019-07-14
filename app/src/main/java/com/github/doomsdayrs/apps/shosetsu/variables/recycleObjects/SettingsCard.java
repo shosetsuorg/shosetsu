4ff49d47a2093cc780daa44909de99d1a95a55c8
doomsdayrs <rahim13657>
2019-07-14 10:41:46 -0400

    Fixed foobar issue, Closes #18

--- app/src/main/java/com/github/doomsdayrs/apps/shosetsu/variables/recycleObjects/SettingsCard.java
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/variables/recycleObjects/SettingsCard.java
@@ -9,7 +9,7 @@ import com.github.doomsdayrs.apps.shosetsu.variables.enums.Types;
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * Foobar is distributed in the hope that it will be useful,
+ * Shosetsu is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
