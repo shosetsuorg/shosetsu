26343112c054a22f0ab06db098ee92129f01b022
Harsh Parekh <h.x.dev@outlook.com>
2019-07-18 00:17:33 -0400

    Revamp Settings_Credit
    Multiple cahnges:
    + Refactor settings_credit to settings_info
    + Create SettingsItem for a general purpose settings
    + Setup listeners for all options in Settings_info

---
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/main/settings/InfoSettings.java
@@ -0,0 +1,95 @@
+package com.github.doomsdayrs.apps.shosetsu.ui.main.settings;
+/*
+ * This file is part of Shosetsu.
+ * Shosetsu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Shosetsu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Shosetsu.  If not, see https://www.gnu.org/licenses/ .
+ * ====================================================================
+ * Shosetsu
+ * 13 / 07 / 2019
+ *
+ * @author github.com/doomsdayrs
+ */
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.doomsdayrs.apps.shosetsu.R;
+
+public class InfoSettings extends Fragment {
+
+    private void onClickAppVer(View v){
+        // TODO: Check for update
+        Toast.makeText(v.getContext(), "AppVer", Toast.LENGTH_SHORT).show();
+    }
+
+    private void onClickReportBug(View v){
+        // TODO: Display the link after consultation
+        Toast.makeText(v.getContext(), "ReportBug", Toast.LENGTH_SHORT).show();
+    }
+
+    private void onClickAuthor(View v){
+        // TODO: Add link to github
+        Toast.makeText(v.getContext(), "Author", Toast.LENGTH_SHORT).show();
+    }
+
+    private void onClickDisclaimer(View v){
+        // TODO: Show full disclaimer on click
+        Toast.makeText(v.getContext(), "Disclaimer", Toast.LENGTH_SHORT).show();
+    }
+
+    private void onClickLicense(View v){
+         // TODO: Show full license on click
+        Toast.makeText(v.getContext(), "License", Toast.LENGTH_SHORT).show();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d("OnCreateView", "ViewSettings");
+        View settingsInfoView = inflater.inflate(R.layout.settings_info, container, false);
+
+        // Setup App version
+        SettingsItem appVerItem = new SettingsItem(settingsInfoView.findViewById(R.id.settings_info_app_version));
+        appVerItem.setTitle(R.string.version);
+        appVerItem.setOnClickListener(this::onClickAppVer);
+        // TODO: Add the app version number after consultation
+
+        // Setup Report Bug
+        SettingsItem reportBugItem = new SettingsItem(settingsInfoView.findViewById(R.id.settings_info_report_bug));
+        reportBugItem.setTitle(R.string.report_bug);
+        reportBugItem.setOnClickListener(this::onClickReportBug);
+
+        // Setup Author
+        SettingsItem authorItem = new SettingsItem(settingsInfoView.findViewById(R.id.settings_info_author));
+        authorItem.setTitle(R.string.author);
+        authorItem.setDesc(R.string.author_name);
+        authorItem.setOnClickListener(this::onClickAuthor);
+
+        // Setup Disclaimer
+        SettingsItem disclaimerItem = new SettingsItem(settingsInfoView.findViewById(R.id.settings_info_disclaimer));
+        disclaimerItem.setTitle(R.string.disclaimer);
+        disclaimerItem.setOnClickListener(this::onClickDisclaimer);
+
+        // Setup License
+        SettingsItem licenseItem = new SettingsItem(settingsInfoView.findViewById(R.id.settings_info_license));
+        licenseItem.setTitle(R.string.license);
+        licenseItem.setOnClickListener(this::onClickLicense);
+
+        return settingsInfoView;
+    }
+}
