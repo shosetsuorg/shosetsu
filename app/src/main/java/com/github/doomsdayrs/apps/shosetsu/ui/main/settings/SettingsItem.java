e7c4c8ab8b90e53120b10db82c9f2a13ebb1ab54
Harsh Parekh <h.x.dev@outlook.com>
2019-07-19 18:14:51 -0400

    Fix onClick for NightMode

--- app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/main/settings/SettingsItem.java
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/main/settings/SettingsItem.java
@@ -42,6 +42,10 @@ public class SettingsItem {
         }
     }
 
+    public void invalidate(){
+        itemView.invalidate();
+    }
+
     public void setTitle(int titleResid) {
         itemTitle.setText(titleResid);
     }
