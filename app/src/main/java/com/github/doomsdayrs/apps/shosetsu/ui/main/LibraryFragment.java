4bb68e602735647a6ab9690048ea6e24e16e8301
doomsdayrs <rahim13657>
2019-07-19 10:28:47 -0400

    Merged branches

--- app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/main/LibraryFragment.java
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/main/LibraryFragment.java
@@ -11,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
