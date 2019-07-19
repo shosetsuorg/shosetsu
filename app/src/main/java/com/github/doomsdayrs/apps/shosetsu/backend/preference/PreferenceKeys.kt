d88978102782abd8d3cd1e1e47ae70d26387b3e1
Harsh Parekh <h.x.dev@outlook.com>
2019-07-19 14:54:01 -0400

    Initial Commit for Preferences
    + Add dependency:
        - kotlin
        - preferences
        - rx preferences
    
    + Add files
        - PreferenceKeys
        - PreferencesHelper

---
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/backend/preference/PreferenceKeys.kt
@@ -0,0 +1,11 @@
+package com.github.doomsdayrs.apps.shosetsu.backend.preference
+
+object PreferenceKeys {
+    // Global Preferences
+    const val theme = "pref_theme"
+
+    // Reader Preferences
+    const val reader_night_mode = "pref_reader_night_mode"
+    const val reader_para_spacing = "reader_para_spacing"
+    const val reader_para_indent = "reader_para_indent"
+}
