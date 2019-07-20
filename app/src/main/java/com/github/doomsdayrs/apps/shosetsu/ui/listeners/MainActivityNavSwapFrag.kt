3c43a57bdc3126f5594cf42c779ffef78486e021
Harsh Parekh <h.x.dev@outlook.com>
2019-07-19 22:46:06 -0400

    Refactor out repetitive code

--- app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/listeners/MainActivityNavSwapFrag.kt
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/listeners/MainActivityNavSwapFrag.kt
@@ -3,9 +3,11 @@ package com.github.doomsdayrs.apps.shosetsu.ui.listeners
 import android.util.Log
 import android.view.MenuItem
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 
 import com.github.doomsdayrs.apps.shosetsu.MainActivity
 import com.github.doomsdayrs.apps.shosetsu.R
+import com.github.doomsdayrs.apps.shosetsu.ui.main.catalogue.CatalogueFragment
 import com.github.doomsdayrs.apps.shosetsu.ui.main.catalogue.CataloguesFragment
 import com.google.android.material.navigation.NavigationView
 
@@ -34,48 +36,24 @@ import com.google.android.material.navigation.NavigationView
  */
 class MainActivityNavSwapFrag(private val mainActivity: MainActivity) : NavigationView.OnNavigationItemSelectedListener {
 
+    private fun setRoot(fragment: Fragment, name: String = "tag") {
+        mainActivity.supportFragmentManager
+                .beginTransaction()
+                .addToBackStack(name)
+                .replace(R.id.fragment_container, fragment)
+    }
+
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         mainActivity.navigationView.setCheckedItem(menuItem)
-        when (menuItem.itemId) {
-            R.id.nav_library -> {
-                Log.d("Nav", "Library selected")
-                mainActivity.supportFragmentManager.beginTransaction()
-                        .addToBackStack("tag")
-                        .replace(R.id.fragment_container, mainActivity.libraryFragment)
-                        .commit()
-            }
-            R.id.nav_catalogue -> {
-                Log.d("Nav", "Catalogue selected")
-                mainActivity.supportFragmentManager.beginTransaction()
-                        .addToBackStack("tag")
-                        .replace(R.id.fragment_container, CataloguesFragment())
-                        .commit()
-            }
-            R.id.nav_settings -> {
-                Log.d("Nav", "Settings selected")
-                mainActivity.supportFragmentManager
-                        .beginTransaction()
-                        .addToBackStack("tag")
-                        .replace(R.id.fragment_container, mainActivity.settingsFragment)
-                        .commit()
-            }
-            R.id.nav_downloads -> {
-                Log.d("Nav", "Downloads Selected")
-                mainActivity.supportFragmentManager
-                        .beginTransaction()
-                        .addToBackStack("tag")
-                        .replace(R.id.fragment_container, mainActivity.downloadsFragment)
-                        .commit()
-            }
-            R.id.nav_updater -> {
-                Log.d("Nav", "Updater Selected")
-                mainActivity.supportFragmentManager
-                        .beginTransaction()
-                        .addToBackStack("tag")
-                        .replace(R.id.fragment_container, mainActivity.updatesFragment)
-                        .commit()
-            }
+        val nextFragment : Fragment = when (menuItem.itemId) {
+            R.id.nav_library -> mainActivity.libraryFragment
+            R.id.nav_updater -> mainActivity.updatesFragment
+            R.id.nav_catalogue -> CatalogueFragment()
+            R.id.nav_settings -> mainActivity.settingsFragment
+            R.id.nav_downloads -> mainActivity.downloadsFragment
+            else -> throw Exception("Invalid navigation item selected.")
         }
+        setRoot(nextFragment)
         mainActivity.drawerLayout.closeDrawer(GravityCompat.START)
 
         return true
