17d08c6d8de5b6536248d94e88fa5bf6b143da61
Harsh Parekh <h.x.dev@outlook.com>
2019-07-21 04:14:38 -0400

    Towards more idiomatic Kotlin

--- app/src/main/java/com/github/doomsdayrs/apps/shosetsu/MainActivity.kt
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/MainActivity.kt
@@ -6,22 +6,24 @@ import android.net.ConnectivityManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 
 import com.github.doomsdayrs.apps.shosetsu.backend.Download_Manager
 import com.github.doomsdayrs.apps.shosetsu.backend.SettingsController
 import com.github.doomsdayrs.apps.shosetsu.backend.database.DBHelper
 import com.github.doomsdayrs.apps.shosetsu.backend.database.Database
-import com.github.doomsdayrs.apps.shosetsu.ui.listeners.MainActivityNavSwapFrag
+import com.github.doomsdayrs.apps.shosetsu.backend.preference.PreferencesHelper
 import com.github.doomsdayrs.apps.shosetsu.ui.main.DownloadsFragment
 import com.github.doomsdayrs.apps.shosetsu.ui.main.LibraryFragment
 import com.github.doomsdayrs.apps.shosetsu.ui.main.settings.SettingsMainFragment
 import com.github.doomsdayrs.apps.shosetsu.ui.main.UpdatesFragment
+import com.github.doomsdayrs.apps.shosetsu.ui.main.catalogue.CataloguesFragment
 import com.github.doomsdayrs.apps.shosetsu.variables.Settings
 import com.github.doomsdayrs.apps.shosetsu.variables.Statics
 import com.github.javiersantos.appupdater.AppUpdater
@@ -31,6 +33,12 @@ import com.github.javiersantos.appupdater.enums.Display
 import com.github.javiersantos.appupdater.enums.UpdateFrom
 import com.github.javiersantos.appupdater.objects.Update
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_main.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -56,14 +64,15 @@ import com.google.android.material.navigation.NavigationView
  * @author github.com/doomsdayrs
  */
 
-class MainActivity : AppCompatActivity() {
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var navigationView: NavigationView
+class MainActivity : AppCompatActivity(), KodeinAware {
+    override val kodein: Kodein by kodein()
 
+    val preferences: PreferencesHelper by instance()
     val libraryFragment = LibraryFragment()
     val updatesFragment = UpdatesFragment()
     val settingsMainFragment = SettingsMainFragment()
     val downloadsFragment = DownloadsFragment()
+    val cataloguesFragment = CataloguesFragment()
 
 
     /**
@@ -72,21 +81,55 @@ class MainActivity : AppCompatActivity() {
      * @param savedInstanceState savedData from destruction
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        //
+
+        setTheme(when (preferences.theme()) {
+            0 -> R.style.Theme_MaterialComponents_Light_NoActionBar
+            1 -> R.style.Theme_MaterialComponents_NoActionBar
+            2 -> R.style.ThemeOverlay_MaterialComponents_Dark
+            else -> R.style.Theme_MaterialComponents_Light_NoActionBar // Default Theme
+        })
+        //  getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
         super.onCreate(savedInstanceState)
+
+        //Set the content view
+        setContentView(R.layout.activity_main)
+
+        //Sets the toolbar
+        setSupportActionBar(toolbar)
+        // TODO: Try to eliminate the statement below
+        Statics.mainActionBar = supportActionBar
+
+        //Sets up the sidebar
+        nav_view.setNavigationItemSelectedListener { item : MenuItem ->
+            nav_view.setCheckedItem(item)
+            val nextFragment = when (item.itemId) {
+                R.id.nav_library -> libraryFragment
+                R.id.nav_updater -> updatesFragment
+                R.id.nav_catalogue -> cataloguesFragment
+                R.id.nav_settings -> settingsMainFragment
+                R.id.nav_downloads -> downloadsFragment
+                else -> throw Exception("Invalid navigation item selected.")
+            }
+            setRoot(nextFragment)
+
+            drawer_layout.closeDrawer(GravityCompat.START)
+            return@setNavigationItemSelectedListener true
+        }
+
+        // TODO: See if this can be removed
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // TODO: Aim to remove all these
         SettingsController.view = getSharedPreferences("view", 0)
         SettingsController.download = getSharedPreferences("download", 0)
         SettingsController.advanced = getSharedPreferences("advanced", 0)
         SettingsController.tracking = getSharedPreferences("tracking", 0)
         SettingsController.backup = getSharedPreferences("backup", 0)
         SettingsController.init()
-
-        when (Settings.themeMode) {
-            0 -> setTheme(R.style.Theme_MaterialComponents_Light_NoActionBar)
-            1 -> setTheme(R.style.Theme_MaterialComponents_NoActionBar)
-            2 -> setTheme(R.style.ThemeOverlay_MaterialComponents_Dark)
-        }
-        //  getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         Log.d("Updater", "Start")
         val appUpdater = AppUpdater(this)
@@ -136,22 +179,6 @@ class MainActivity : AppCompatActivity() {
         // Settings setup
         Settings.connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        //Set the content view
-        setContentView(R.layout.activity_main)
-
-        //Sets the toolbar
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        Statics.mainActionBar = supportActionBar
-        //Sets up the sidebar
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(MainActivityNavSwapFrag(this))
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
         // Sets up DB
         val helper = DBHelper(this)
         Database.library = helper.writableDatabase
@@ -163,16 +190,24 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.fragment_container, libraryFragment).commit()
-            navigationView.setCheckedItem(R.id.nav_library)
+            nav_view.setCheckedItem(R.id.nav_library)
         }
+    }
+
+    private fun setRoot(fragment: Fragment, name: String = "tag") {
+        supportFragmentManager
+                .beginTransaction()
+                .addToBackStack(name)
+                .replace(R.id.fragment_container, fragment)
+                .commit()
     }
 
     /**
      * When the back button while drawer is open, close it.
      */
     override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START))
-            drawerLayout.closeDrawer(GravityCompat.START)
+        if (drawer_layout.isDrawerOpen(GravityCompat.START))
+            drawer_layout.closeDrawer(GravityCompat.START)
         else
             super.onBackPressed()
     }
