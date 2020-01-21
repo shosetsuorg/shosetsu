85d8cee8b0d8f71a6eb69f62ff871123af6a54b8
doomsdayrs <rahim13657>
2020-01-21 10:39:23 -0500

    Action All for sus script dialog added

--- app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/susScript/SusScriptDialog.kt
+++ app/src/main/java/com/github/doomsdayrs/apps/shosetsu/ui/susScript/SusScriptDialog.kt
@@ -3,6 +3,8 @@ package com.github.doomsdayrs.apps.shosetsu.ui.susScript
 import android.app.Activity
 import android.app.AlertDialog
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.doomsdayrs.api.shosetsu.services.core.dep.LuaFormatter
@@ -38,6 +40,42 @@ import java.io.File
  */
 class SusScriptDialog(val activity: Activity, fileList: ArrayList<File>) {
 
+    class Dialog(inflater: LayoutInflater, private val builder: AlertDialog.Builder, private val susScriptDialog: SusScriptDialog) {
+        val view = inflater.inflate(R.layout.alert_extensions_handle, null)!!
+        lateinit var dialog: AlertDialog
+
+        init {
+            val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+            recyclerView.adapter = SusScriptAdapter(susScriptDialog)
+            recyclerView.layoutManager = LinearLayoutManager(builder.context, LinearLayoutManager.VERTICAL, false)
+
+            val acceptAll: Button = view.findViewById(R.id.acceptAll)
+            acceptAll.setOnClickListener {
+                susScriptDialog.setAll(0)
+                susScriptDialog.processActions()
+                dialog.hide()
+            }
+            val tempAll: Button = view.findViewById(R.id.tempAll)
+            tempAll.setOnClickListener {
+                susScriptDialog.setAll(1)
+                susScriptDialog.processActions()
+                dialog.hide()
+            }
+            val disableAll: Button = view.findViewById(R.id.disableAll)
+            disableAll.setOnClickListener {
+                susScriptDialog.setAll(2)
+                susScriptDialog.processActions()
+                dialog.hide()
+            }
+            val uninstallAll: Button = view.findViewById(R.id.uninstallAll)
+            uninstallAll.setOnClickListener {
+                susScriptDialog.setAll(3)
+                susScriptDialog.processActions()
+                dialog.hide()
+            }
+        }
+    }
+
     class FileObject(val file: File) {
         /**
          * 0: Accept, 1: Temp Accept, 2: Disable, 3: Remove
@@ -62,33 +100,11 @@ class SusScriptDialog(val activity: Activity, fileList: ArrayList<File>) {
         builder.setTitle(activity.getString(R.string.sus_script_title))
         builder.setMessage(R.string.sus_scripts)
 
-        val inflater = activity.layoutInflater
-        val view = inflater.inflate(R.layout.alert_extensions_handle, null)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = SusScriptAdapter(this)
-        recyclerView.layoutManager = LinearLayoutManager(builder.context, LinearLayoutManager.VERTICAL, false)
-
-        builder.setView(view)
+        val dialog = Dialog(activity.layoutInflater, builder, this)
+        builder.setView(dialog.view)
 
         builder.setPositiveButton(android.R.string.yes) { _, _ ->
-            for (file in files) {
-                Log.i("SusScriptDialog", "File confirmed Action\t${file.file.name}\t${file.action}")
-                when (file.action) {
-                    0 -> {
-                        FormatterController.trustScript(file.file)
-                        DefaultScrapers.formatters.add(LuaFormatter(file.file))
-                    }
-                    1 -> {
-                        DefaultScrapers.formatters.add(LuaFormatter(file.file))
-                    }
-                    2 -> {
-                        //TODO ignore list
-                    }
-                    3 -> {
-                        file.delete()
-                    }
-                }
-            }
+            processActions()
         }
 
         builder.setNegativeButton(android.R.string.cancel) { _, _ ->
@@ -97,6 +113,32 @@ class SusScriptDialog(val activity: Activity, fileList: ArrayList<File>) {
                 file.delete()
             }
         }
-        builder.show()
+        dialog.dialog = builder.show()
+    }
+
+    fun processActions() {
+        for (file in files) {
+            Log.i("SusScriptDialog", "File confirmed Action\t${file.file.name}\t${file.action}")
+            when (file.action) {
+                0 -> {
+                    FormatterController.trustScript(file.file)
+                    DefaultScrapers.formatters.add(LuaFormatter(file.file))
+                }
+                1 -> {
+                    DefaultScrapers.formatters.add(LuaFormatter(file.file))
+                }
+                2 -> {
+                    //TODO ignore list
+                }
+                3 -> {
+                    file.delete()
+                }
+            }
+        }
+    }
+
+    fun setAll(int: Int) {
+        for (file in files)
+            file.action = int
     }
 }
