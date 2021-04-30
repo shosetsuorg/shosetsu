504b706a4875e860aba086e05a1cc3fc29853f35
doomsdayrs <doomsdayrs@gmail.com>
2021-04-30 13:42:18 -0400

    Updated dependencies & Working to migrate android gradle to kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,9 +11,12 @@ java {
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+
 	implementation("com.github.shosetsuorg:kotlin-lib:1.0.0-rc55")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+
 	testImplementation(kotlin("test"))
 	testImplementation(kotlin("test-junit"))
 	implementation(kotlin("reflect"))
