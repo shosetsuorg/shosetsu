b2112564e8ff71e324a0dcc59584633ae4cab512
doomsdayrs <doomsdayrs@gmail.com>
2021-06-26 23:59:13 -0400

    Add EmptyDataView & ProgressBar (settings) in ConfigureExtension.kt
    This is a mostly cosmetic update.
    It informs the user that there can be settings for an Extension.
    For slower devices, Progress indicator for the loading of the settings.
    
    Revealed an issue that on first boot, the ViewModel wont emit empty.
    This is fixed with an auto check after 200ms to reload.

--- android/src/main/java/app/shosetsu/android/ui/extensionsConfigure/ConfigureExtension.kt
+++ android/src/main/java/app/shosetsu/android/ui/extensionsConfigure/ConfigureExtension.kt
@@ -3,9 +3,9 @@ package app.shosetsu.android.ui.extensionsConfigure
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.view.isVisible
 import app.shosetsu.android.common.consts.BundleKeys.BUNDLE_EXTENSION
-import app.shosetsu.android.common.ext.picasso
-import app.shosetsu.android.common.ext.viewModel
+import app.shosetsu.android.common.ext.*
 import app.shosetsu.android.view.controller.FastAdapterRecyclerController
 import app.shosetsu.android.view.controller.base.CollapsedToolBarController
 import app.shosetsu.android.view.uimodels.model.ExtensionUI
@@ -15,6 +15,7 @@ import app.shosetsu.common.dto.HResult
 import app.shosetsu.common.dto.handle
 import com.github.doomsdayrs.apps.shosetsu.R
 import com.github.doomsdayrs.apps.shosetsu.databinding.ConfigureExtensionViewBinding
+import kotlinx.coroutines.delay
 
 /*
  * This file is part of shosetsu.
@@ -46,12 +47,29 @@ class ConfigureExtension(bundle: Bundle) :
 
 	override fun onViewCreated(view: View) {
 		viewModel.setExtensionID(args.getInt(BUNDLE_EXTENSION))
-		observe()
+
+		var reloaded = false
+		// Due to an obscure issue, the follow check is made and will reload the extension if there is no entries
+		launchIO {
+			delay(200)
+			if (itemAdapter.adapterItemCount == 0 && !reloaded && !binding.emptyDataView.isVisible) {
+				reloaded = true
+				launchUI {
+					viewModel.destroy()
+					viewModel.setExtensionID(args.getInt(BUNDLE_EXTENSION))
+				}
+			}
+		}
+
+		observeChanges()
 	}
 
-	private fun observe() {
-		viewModel.liveData.observe(this) { handleExtensionResult(it) }
-		viewModel.extensionSettings.observe(this) { handleRecyclerUpdate(it) }
+	private fun observeChanges() {
+		viewModel.liveData.observe { handleExtensionResult(it) }
+		viewModel.extensionSettings.observe {
+			logV("Received settings result $it")
+			handleRecyclerUpdate(it)
+		}
 	}
 
 	private fun handleExtensionResult(result: HResult<ExtensionUI>) =
@@ -77,6 +95,19 @@ class ConfigureExtension(bundle: Bundle) :
 			}
 		}
 
+	override fun showEmpty() {
+		binding.emptyDataView.show(R.string.controller_configure_extension_empty_settings)
+	}
+
+	override fun showLoading() {
+		binding.settingsProgressBar.isVisible = true
+	}
+
+	override fun hideEmpty() {
+		binding.settingsProgressBar.isVisible = false
+		binding.emptyDataView.hide()
+	}
+
 	override fun onDestroy() {
 		super.onDestroy()
 		viewModel.destroy()
@@ -85,7 +116,13 @@ class ConfigureExtension(bundle: Bundle) :
 	override fun bindView(inflater: LayoutInflater): ConfigureExtensionViewBinding =
 		ConfigureExtensionViewBinding.inflate(inflater).also { recyclerView = it.settings }
 
+	override fun updateUI(newList: List<SettingsItemData>) {
+		logD("updateUI with ${newList.size}")
+		super.updateUI(newList)
+	}
+
 	override fun handleErrorResult(e: HResult.Error) {
-		TODO("Not yet implemented")
+		logW("handleErrorResult invoked")
+		e.exception?.printStackTrace()
 	}
 }
