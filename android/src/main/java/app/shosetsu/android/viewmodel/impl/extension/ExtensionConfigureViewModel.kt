3367c5b18be8da70cc0a034d2b6ffcb8cd09e46e
doomsdayrs <doomsdayrs@gmail.com>
2021-07-21 14:58:08 -0400

    Fix configure extension ui not showing listing item
    The issue seems to stem from how the flows are synced together.
    By separating the two flows from each other, then combining them again.
    
    - Also renamed some values

--- android/src/main/java/app/shosetsu/android/viewmodel/impl/extension/ExtensionConfigureViewModel.kt
+++ android/src/main/java/app/shosetsu/android/viewmodel/impl/extension/ExtensionConfigureViewModel.kt
@@ -19,17 +19,14 @@ package app.shosetsu.android.viewmodel.impl.extension
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import app.shosetsu.android.common.ext.launchIO
-import app.shosetsu.android.common.ext.logI
-import app.shosetsu.android.common.ext.logV
+import app.shosetsu.android.common.ext.*
 import app.shosetsu.android.domain.ReportExceptionUseCase
 import app.shosetsu.android.domain.usecases.UninstallExtensionUIUseCase
-import app.shosetsu.android.domain.usecases.get.GetExtListingNamessUseCase
+import app.shosetsu.android.domain.usecases.get.GetExtListingNamesUseCase
 import app.shosetsu.android.domain.usecases.get.GetExtSelectedListingUseCase
 import app.shosetsu.android.domain.usecases.get.GetExtensionSettingsUseCase
 import app.shosetsu.android.domain.usecases.get.GetExtensionUIUseCase
 import app.shosetsu.android.domain.usecases.update.UpdateExtSelectedListing
-import app.shosetsu.android.domain.usecases.update.UpdateExtensionEntityUseCase
 import app.shosetsu.android.view.uimodels.model.ExtensionUI
 import app.shosetsu.android.view.uimodels.settings.base.SettingsItemData
 import app.shosetsu.android.view.uimodels.settings.dsl.*
@@ -47,69 +44,80 @@ import kotlinx.coroutines.flow.*
  */
 class ExtensionConfigureViewModel(
 	private val application: Application,
-	private val loadExtensionUIUI: GetExtensionUIUseCase,
-	private val updateExtensionEntityUseCase: UpdateExtensionEntityUseCase,
-	private val uninstallExtensionUIUseCase: UninstallExtensionUIUseCase,
+	private val loadExtensionUI: GetExtensionUIUseCase,
+	private val uninstallExtensionUI: UninstallExtensionUIUseCase,
 	private val getExtensionSettings: GetExtensionSettingsUseCase,
-	private val reportExceptionUseCase: ReportExceptionUseCase,
-	private val getExtListNames: GetExtListingNamessUseCase,
+	private val reportException: ReportExceptionUseCase,
+	private val getExtListNames: GetExtListingNamesUseCase,
 	private val updateExtSelectedListing: UpdateExtSelectedListing,
 	private val getExtSelectedListing: GetExtSelectedListingUseCase
 ) : AExtensionConfigureViewModel() {
-	private val extensionIDFlow: MutableStateFlow<Int> by lazy {
-		MutableStateFlow(-1)
-	}
+	private val extensionIdFlow: MutableStateFlow<Int> by lazy { MutableStateFlow(-1) }
 
 	@ExperimentalCoroutinesApi
 	override val liveData: LiveData<HResult<ExtensionUI>> by lazy {
-		extensionIDFlow.transformLatest { id ->
-			emitAll(loadExtensionUIUI(id))
+		extensionIdFlow.transformLatest { id ->
+			emitAll(loadExtensionUI(id))
 		}.asIOLiveData()
 	}
 
 	override fun reportError(error: HResult.Error, isSilent: Boolean) {
-		reportExceptionUseCase(error)
+		reportException(error)
+	}
+
+	private suspend fun makeListingSetting(extensionID: Int, nameList: List<String>) =
+		spinnerSettingData(0) {
+			titleRes = R.string.listings
+			getExtSelectedListing(extensionID).handle { selectedListing ->
+				spinnerValue { selectedListing }
+			}
+			arrayAdapter = android.widget.ArrayAdapter(
+				application.applicationContext,
+				android.R.layout.simple_spinner_dropdown_item,
+				nameList.toTypedArray()
+			)
+			var first = true
+			onSpinnerItemSelected { _, _, position, _ ->
+				if (first) {
+					first = false
+					return@onSpinnerItemSelected
+				}
+				launchIO {
+					updateExtSelectedListing(extensionID, position)
+				}
+			}
+		}
+
+	@ExperimentalCoroutinesApi
+	private val extListNamesFlow: Flow<HResult<List<SettingsItemData>>> by lazy {
+		extensionIdFlow.transformLatest { extensionID ->
+			emit(
+				getExtListNames(extensionID).transformToSuccess { nameList ->
+					listOf(makeListingSetting(extensionID, nameList))
+				}
+			)
+		}
+	}
+
+	@ExperimentalCoroutinesApi
+	private val extensionSettingsFlow: Flow<HResult<List<SettingsItemData>>> by lazy {
+		extensionIdFlow.transformLatest { extensionID ->
+			emitAll(getExtensionSettings(extensionID))
+		}
 	}
 
 	@ExperimentalCoroutinesApi
 	override val extensionSettings: LiveData<HResult<List<SettingsItemData>>> by lazy {
-		extensionIDFlow.transformLatest { extensionID ->
-			getExtListNames(extensionID).handle(
-				onError = { emit(it) },
-				onEmpty = { emit(empty) },
-				onLoading = { emit(loading) }
-			) { nameList ->
-				emitAll(
-					getExtensionSettings(extensionID).mapLatestResult { extensionSettings ->
-						successResult(
-							ArrayList(extensionSettings).apply {
-								if (nameList.size > 1) {
-									add(0, spinnerSettingData(0) {
-										titleRes = R.string.listings
-										getExtSelectedListing(extensionID).handle { selectedListing ->
-											spinnerValue { selectedListing }
-										}
-										arrayAdapter = android.widget.ArrayAdapter(
-											application.applicationContext,
-											android.R.layout.simple_spinner_dropdown_item,
-											nameList.toTypedArray()
-										)
-										var first = true
-										onSpinnerItemSelected { _, _, position, _ ->
-											if (first) {
-												first = false
-												return@onSpinnerItemSelected
-											}
-											launchIO {
-												updateExtSelectedListing(extensionID, position)
-											}
-										}
-									})
-								}
-							}
-						)
-					}
-				)
+		extListNamesFlow.combine(extensionSettingsFlow) { a, b ->
+			logD("Listing result: $a")
+			logD("Settings result: $b")
+			val listings = a.unwrap() ?: listOf()
+			b.transform(
+				onEmpty = {
+					successResult(listings)
+				},
+			) {
+				successResult(listings + it)
 			}
 		}.asIOLiveData()
 	}
@@ -118,19 +126,19 @@ class ExtensionConfigureViewModel(
 		logV("Setting extension id = $id")
 		launchIO {
 			when {
-				extensionIDFlow.value == id -> {
+				extensionIdFlow.value == id -> {
 					this@ExtensionConfigureViewModel.logI("id is the same, ignoring")
 					return@launchIO
 				}
-				extensionIDFlow.value != id -> {
+				extensionIdFlow.value != id -> {
 					this@ExtensionConfigureViewModel.logI("id is different, resetting")
 					destroy()
 				}
-				extensionIDFlow.value == -1 -> {
+				extensionIdFlow.value == -1 -> {
 					this@ExtensionConfigureViewModel.logI("id is new, setting")
 				}
 			}
-			extensionIDFlow.value = id
+			extensionIdFlow.value = id
 		}
 	}
 
@@ -139,7 +147,7 @@ class ExtensionConfigureViewModel(
 	}
 
 	override fun destroy() {
-		extensionIDFlow.value = -1
+		extensionIdFlow.value = -1
 	}
 }
 
