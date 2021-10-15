5b54484bd51c8bb096b180519be53e7837381f55
doomsdayrs <doomsdayrs@gmail.com>
2021-10-14 22:35:18 -0400

    Cut out old classes and xml documents
    These are quite obsolete now that compose is moving into shosetsu

--- android/src/main/java/app/shosetsu/android/view/uimodels/model/UpdateUI.kt
+++ android/src/main/java/app/shosetsu/android/view/uimodels/model/UpdateUI.kt
@@ -1,15 +1,5 @@
 package app.shosetsu.android.view.uimodels.model
 
-import android.text.format.DateFormat.format
-import android.view.View
-import androidx.core.view.isVisible
-import app.shosetsu.android.common.ext.shosetsuLoad
-import app.shosetsu.android.view.uimodels.base.BaseRecyclerItem
-import app.shosetsu.android.view.uimodels.base.BindViewHolder
-import com.github.doomsdayrs.apps.shosetsu.R
-import com.github.doomsdayrs.apps.shosetsu.databinding.RecyclerUpdateUiBinding
-import java.util.*
-
 /*
  * This file is part of shosetsu.
  *
@@ -33,6 +23,13 @@ import java.util.*
  *
  * UpdateUI entity to display
  */
+@Deprecated(
+	"Remove for usage with FastAdapter is obsolete",
+	ReplaceWith(
+		"UpdateCompleteEntity",
+		"app.shosetsu.common.domain.model.local.UpdateCompleteEntity"
+	)
+)
 data class UpdateUI(
 	val chapterID: Int,
 	val novelID: Int,
@@ -40,39 +37,4 @@ data class UpdateUI(
 	val chapterName: String,
 	val novelName: String,
 	val novelImageURL: String,
-) : BaseRecyclerItem<UpdateUI.ViewHolder>() {
-	override val layoutRes: Int = R.layout.recycler_update_ui
-	override val type: Int = R.layout.recycler_update_ui
-	override var identifier: Long
-		get() = chapterID.toLong()
-		set(_) {}
-
-	override fun getViewHolder(v: View): ViewHolder = ViewHolder(v)
-
-	/**
-	 * ViewHolder for [UpdateUI]
-	 */
-	class ViewHolder(itemView: View) : BindViewHolder<UpdateUI, RecyclerUpdateUiBinding>(itemView) {
-		override val binding = RecyclerUpdateUiBinding.bind(view)
-
-		override fun RecyclerUpdateUiBinding.bindView(item: UpdateUI, payloads: List<Any>) {
-			if (item.novelImageURL.isNotEmpty()) {
-				imageView.shosetsuLoad(item.novelImageURL)
-			} else {
-				novelTitle.isVisible = true
-				novelTitle.text = item.novelName
-			}
-
-			chapterTitle.text = item.chapterName
-			date.text = format("hh:mm", Date(item.time))
-		}
-
-		override fun RecyclerUpdateUiBinding.unbindView(item: UpdateUI) {
-			imageView.setImageResource(R.drawable.broken_image)
-			chapterTitle.text = null
-			novelTitle.isVisible = false
-			novelTitle.text = null
-			date.text = null
-		}
-	}
-}
+)
