c27fe99445f2db1cab27b547f5a5b11a372e1e6c
doomsdayrs <doomsdayrs@gmail.com>
2021-10-21 01:19:58 -0400

    Convert AboutController.kt to compose

--- android/src/main/java/app/shosetsu/android/ui/about/AboutController.kt
+++ android/src/main/java/app/shosetsu/android/ui/about/AboutController.kt
@@ -1,64 +1,105 @@
 package app.shosetsu.android.ui.about
 
 import android.content.Intent
-import android.content.Intent.ACTION_VIEW
 import android.net.Uri
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import app.shosetsu.android.common.enums.TextAsset.DISCLAIMER
-import app.shosetsu.android.common.enums.TextAsset.LICENSE
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.shosetsu.android.common.enums.TextAsset
 import app.shosetsu.android.common.ext.shosetsuPush
 import app.shosetsu.android.common.ext.viewModel
 import app.shosetsu.android.ui.settings.sub.TextAssetReader
-import app.shosetsu.android.view.controller.ViewedController
+import app.shosetsu.android.view.controller.ShosetsuController
 import app.shosetsu.android.viewmodel.abstracted.AAboutViewModel
 import app.shosetsu.common.consts.*
-import app.shosetsu.common.dto.HResult
 import com.github.doomsdayrs.apps.shosetsu.BuildConfig
 import com.github.doomsdayrs.apps.shosetsu.R
-import com.github.doomsdayrs.apps.shosetsu.databinding.ControllerAboutBinding
+import com.google.android.material.composethemeadapter.MdcTheme
 
 /*
- * This file is part of Shosetsu.
+ * This file is part of shosetsu.
  *
- * Shosetsu is free software: you can redistribute it and/or modify
+ * shosetsu is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Shosetsu is distributed in the hope that it will be useful,
+ * shosetsu is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Shosetsu.  If not, see <https://www.gnu.org/licenses/>.
+ * along with shosetsu.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
- * shosetsu
- * 01 / 10 / 2020
+ * Shosetsu
+ *
+ * @since 21 / 10 / 2021
+ * @author Doomsdayrs
  */
-class AboutController : ViewedController<ControllerAboutBinding>() {
+class AboutController : ShosetsuController() {
+
 	override val viewTitleRes: Int = R.string.about
+
 	private val viewModel: AAboutViewModel by viewModel()
 
-	override fun onViewCreated(view: View) {
-		binding.apply {
-			versionTag.text = BuildConfig.VERSION_NAME
-			appUpdateCheck.setOnClickListener { viewModel.appUpdateCheck() }
-			website.setOnClickListener { openWebsite() }
-			github.setOnClickListener { openGithub() }
-			extensions.setOnClickListener { openExtensions() }
-			discord.setOnClickListener { openDiscord() }
-			patreon.setOnClickListener { openPatreon() }
-			sourceLicensesCard.setOnClickListener { onClickLicense() }
-			disclaimerCard.setOnClickListener { onClickDisclaimer() }
+	override fun onViewCreated(view: View) {}
+
+	@ExperimentalMaterialApi
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup,
+		savedViewState: Bundle?
+	): View = ComposeView(container.context).apply {
+		setViewTitle()
+		setContent {
+			MdcTheme {
+				AboutContent(
+					currentVersion = BuildConfig.VERSION_NAME,
+					onCheckForAppUpdate = viewModel::appUpdateCheck,
+					onOpenWebsite = ::openWebsite,
+					onOpenSource = ::openGithub,
+					onOpenExtensions = ::openExtensions,
+					onOpenDiscord = ::openDiscord,
+					onOpenPatreon = ::openPatreon,
+					onOpenLicense = ::onClickLicense,
+					onOpenDisclaimer = ::onClickDisclaimer
+				)
+			}
 		}
 	}
 
+	private fun onClickLicense() =
+		router.shosetsuPush(TextAssetReader(TextAsset.LICENSE))
+
+	private fun onClickDisclaimer() =
+		router.shosetsuPush(TextAssetReader(TextAsset.DISCLAIMER))
+
 	private fun openSite(url: String) {
-		startActivity(Intent(ACTION_VIEW, Uri.parse(url)))
+		startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
 	}
 
 	private fun openWebsite() =
@@ -75,15 +116,130 @@ class AboutController : ViewedController<ControllerAboutBinding>() {
 
 	private fun openGithub() =
 		openSite(URL_GITHUB_APP)
+}
 
-	override fun bindView(inflater: LayoutInflater): ControllerAboutBinding =
-		ControllerAboutBinding.inflate(inflater)
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun PreviewAboutContent() {
+	MdcTheme {
+		AboutContent(
+			currentVersion = BuildConfig.VERSION_NAME,
+			onCheckForAppUpdate = {},
+			onOpenWebsite = {},
+			onOpenSource = {},
+			onOpenExtensions = {},
+			onOpenDiscord = {},
+			onOpenPatreon = {},
+			onOpenLicense = {},
+			onOpenDisclaimer = {}
+		)
+	}
+}
 
-	private fun onClickLicense() =
-		router.shosetsuPush(TextAssetReader(LICENSE))
+@ExperimentalMaterialApi
+@Composable
+fun AboutItem(
+	@StringRes titleRes: Int,
+	@StringRes descriptionRes: Int? = null,
+	description: String? = null,
+	@DrawableRes iconRes: Int? = null,
+	onClick: () -> Unit = {}
+) {
+	Card(
+		onClick = onClick,
+		elevation = 0.dp,
+		shape = RectangleShape,
+	) {
+		Row(
+			verticalAlignment = Alignment.CenterVertically,
+			horizontalArrangement = Arrangement.Start,
+			modifier = Modifier.padding(16.dp)
+		) {
+			if (iconRes != null)
+				Image(painterResource(iconRes), null)
 
-	private fun onClickDisclaimer() =
-		router.shosetsuPush(TextAssetReader(DISCLAIMER))
+			Column(
+				verticalArrangement = Arrangement.Center,
+				horizontalAlignment = Alignment.Start,
+				modifier = Modifier.fillMaxWidth()
+			) {
+				Text(stringResource(titleRes))
 
-	override fun handleErrorResult(e: HResult.Error) {}
+				if (descriptionRes != null)
+					Text(
+						stringResource(descriptionRes),
+						fontSize = dimensionResource(R.dimen.sub_text_size).value.sp,
+						modifier = Modifier.alpha(0.7f)
+					)
+				else if (description != null)
+					Text(
+						description,
+						fontSize = dimensionResource(R.dimen.sub_text_size).value.sp,
+						modifier = Modifier.alpha(0.7f)
+					)
+			}
+		}
+	}
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun AboutContent(
+	currentVersion: String,
+	onCheckForAppUpdate: () -> Unit,
+	onOpenWebsite: () -> Unit,
+	onOpenSource: () -> Unit,
+	onOpenExtensions: () -> Unit,
+	onOpenDiscord: () -> Unit,
+	onOpenPatreon: () -> Unit,
+	onOpenLicense: () -> Unit,
+	onOpenDisclaimer: () -> Unit
+) {
+	Column(
+		modifier = Modifier.fillMaxSize()
+	) {
+		AboutItem(
+			R.string.version,
+			description = currentVersion
+		)
+		AboutItem(
+			R.string.check_for_app_update,
+			onClick = onCheckForAppUpdate
+		)
+		Divider()
+		AboutItem(
+			R.string.website,
+			R.string.website_url,
+			onClick = onOpenWebsite
+		)
+		AboutItem(
+			R.string.github,
+			R.string.github_url,
+			onClick = onOpenSource
+		)
+		AboutItem(
+			R.string.extensions,
+			R.string.extensions_url,
+			onClick = onOpenExtensions
+		)
+		AboutItem(
+			R.string.discord,
+			R.string.discord_url,
+			onClick = onOpenDiscord
+		)
+		AboutItem(
+			R.string.patreon,
+			R.string.patreon_url,
+			onClick = onOpenPatreon
+		)
+		AboutItem(
+			R.string.source_licenses,
+			onClick = onOpenLicense
+		)
+		AboutItem(
+			R.string.disclaimer,
+			onClick = onOpenDisclaimer
+		)
+	}
 }
