87181957a818b984dceaf297d60d50514ee6a89c
doomsdayrs <doomsdayrs@gmail.com>
2021-12-15 13:28:07 -0500

    Upgraded android gradle tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("com.android.tools.build:gradle:7.0.3")
+		classpath("com.android.tools.build:gradle:7.0.4")
 		classpath(kotlin("gradle-plugin", version = kotlinVersion))
 		classpath(kotlin("serialization", version = kotlinVersion))
 		// NOTE: Do not place your application dependencies here; they belong
