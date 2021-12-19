7f802c5ca229bf45a562e1b9fc8ba0cad40b9af7
doomsdayrs <doomsdayrs@gmail.com>
2021-12-19 17:37:05 -0500

    Implement DBRepositoryExtensionEntity.kt

---
+++ android/src/main/java/app/shosetsu/android/domain/model/database/DBRepositoryExtensionEntity.kt
@@ -0,0 +1,82 @@
+package app.shosetsu.android.domain.model.database
+
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import app.shosetsu.lib.ExtensionType
+import app.shosetsu.lib.Version
+
+/*
+ * This file is part of Shosetsu.
+ *
+ * Shosetsu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shosetsu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shosetsu.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * shosetsu
+ * 05 / 12 / 2020
+ *
+ */
+@Entity(
+	tableName = "repository_extension",
+	foreignKeys = [
+		ForeignKey(
+			entity = DBRepositoryEntity::class,
+			parentColumns = ["id"],
+			childColumns = ["repoID"],
+			onDelete = ForeignKey.CASCADE
+		)
+	],
+	indices = [
+		Index("repoID"),
+		Index("id", "repoID", unique = true)
+	]
+)
+
+data class DBRepositoryExtensionEntity(
+	/** Repository extension belongs too*/
+	@NonNull
+	val repoID: Int,
+
+	/** Extension ID */
+	@NonNull
+	val id: Int,
+
+	/** Name of the extension, can be changed */
+	@NonNull
+	var name: String,
+
+	/** FileName of the extension */
+	@NonNull
+	val fileName: String,
+
+	/** Image URL of the extension*/
+	@NonNull
+	var imageURL: String,
+
+	/** The language of the extension */
+	@NonNull
+	val lang: String,
+
+	@NonNull
+	val version: Version,
+
+	/** MD5 to check against */
+	@NonNull
+	var md5: String,
+
+	@NonNull
+	val type: ExtensionType
+)
