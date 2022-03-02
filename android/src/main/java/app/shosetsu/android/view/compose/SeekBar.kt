a1835a170cb8951be89feb8c86e022344c1be4b3
doomsdayrs <doomsdayrs@gmail.com>
2022-03-02 00:36:25 -0500

    Use a "Slider" & "Text" combo for discrete seekbar
    Honestly I give up trying to make a custom slider.
    This is a much easier path.

--- android/src/main/java/app/shosetsu/android/view/compose/SeekBar.kt
+++ android/src/main/java/app/shosetsu/android/view/compose/SeekBar.kt
@@ -1,103 +1,68 @@
 package app.shosetsu.android.view.compose
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Slider
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.google.android.material.composethemeadapter.MdcTheme
+import kotlin.math.roundToInt
 
 @Preview
 @Composable
 fun PreviewSeekBar() {
 	var value by remember { mutableStateOf(1) }
-	Box(
-		modifier = Modifier
-			.fillMaxSize()
-			.padding(16.dp)
-	) {
-		DiscreteSlider(
-			range = 1..5,
-			indexName = {
-				"$it"
-			},
-			value = value,
-			onValueChange = {
-				value = it
-			},
-			showAllIndices = true,
-		)
+	MdcTheme {
+		Box(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(16.dp)
+		) {
+			DiscreteSlider(
+				value,
+				"${value}h",
+				{
+					value = it
+				},
+				0..10,
+			)
+		}
 	}
 }
 
+
+/**
+ * This creates a sudo discrete slider
+ *
+ * @param value Value to set [Slider] to
+ * @param parsedValue [value] parsed to be displayed as a string
+ * @param updateValue Called when [Slider] updates its value, is fed a rounded float
+ * @param valueRange An integer range of possible values
+ */
 @Composable
 fun DiscreteSlider(
-	range: IntRange,
-	indexName: (index: Int) -> String,
 	value: Int,
-	onValueChange: (Int) -> Unit,
-	showAllIndices: Boolean = false,
-	showIndex: ((Int) -> Boolean)? = null,
-	fontSize: TextUnit? = null,
-	textColor: Color? = null
+	parsedValue: String,
+	updateValue: (Int) -> Unit,
+	valueRange: IntRange
 ) {
-	if (!showAllIndices)
-		requireNotNull(showIndex)
-
-	val (sliderValue, setSliderValue) = remember { mutableStateOf(value) }
-	val drawPadding = with(LocalDensity.current) { 10.dp.toPx() }
-	val lineHeightDp = 10.dp
-	val lineHeightPx = with(LocalDensity.current) { lineHeightDp.toPx() }
-	val canvasHeight = 50.dp
-	val textPaint = android.graphics.Paint().apply {
-		color = textColor?.value?.toInt() ?: Color.Gray.value.toInt()
-		textAlign = android.graphics.Paint.Align.CENTER
-		this.textSize = (fontSize?.value ?: 14f)
-	}
-	Box(contentAlignment = Alignment.Center) {
-		Canvas(
-			modifier = Modifier
-				.height(canvasHeight)
-				.fillMaxWidth()
-				.padding(
-					top = canvasHeight
-						.div(2)
-						.minus(lineHeightDp.div(2))
-				)
-		) {
-			val yStart = 0f
-			val distance = (size.width.minus(2 * drawPadding)).div(range.last - 1)
-			range.forEachIndexed { index, step ->
-				drawLine(
-					color = Color.DarkGray,
-					start = Offset(x = drawPadding + index.times(distance), y = yStart),
-					end = Offset(x = drawPadding + index.times(distance), y = lineHeightPx)
-				)
-				if (showAllIndices || showIndex!!.invoke(index)) {
-					this.drawContext.canvas.nativeCanvas.drawText(
-						indexName(step),
-						drawPadding + index.times(distance),
-						textPaint.textSize,
-						textPaint
-					)
-				}
-			}
-		}
+	Row(
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		Text(parsedValue, modifier = Modifier.padding(end = 8.dp))
 		Slider(
-			modifier = Modifier.fillMaxWidth(),
-			value = sliderValue.toFloat(),
-			valueRange = range.first.toFloat()..range.last.toFloat(),
-			steps = range.last.minus(2),
-			onValueChange = {
-				setSliderValue(it.toInt())
-				onValueChange(it.toInt())
-			})
+			value.toFloat(),
+			{
+				updateValue(it.roundToInt())
+			},
+			valueRange = valueRange.first.toFloat()..valueRange.last.toFloat(),
+			steps = valueRange.count() - 2
+		)
 	}
 }
