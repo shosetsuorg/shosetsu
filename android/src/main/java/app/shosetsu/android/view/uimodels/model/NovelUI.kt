a0fbb9a5273abb75d4d7a2f77c05a9f6978946a9
doomsdayrs <doomsdayrs@gmail.com>
2022-04-19 18:22:03 -0400

    Remove old novel ui xml

--- android/src/main/java/app/shosetsu/android/view/uimodels/model/NovelUI.kt
+++ android/src/main/java/app/shosetsu/android/view/uimodels/model/NovelUI.kt
@@ -1,18 +1,8 @@
 package app.shosetsu.android.view.uimodels.model
 
-import android.view.View
-import android.widget.ImageView
-import app.shosetsu.android.common.ext.getString
-import app.shosetsu.android.common.ext.shosetsuLoad
-import app.shosetsu.android.view.uimodels.base.BaseRecyclerItem
-import app.shosetsu.android.view.uimodels.base.BindViewHolder
-import app.shosetsu.android.view.uimodels.model.NovelUI.ViewHolder
 import app.shosetsu.common.domain.model.local.NovelEntity
 import app.shosetsu.common.dto.Convertible
 import app.shosetsu.lib.Novel
-import com.github.doomsdayrs.apps.shosetsu.R
-import com.github.doomsdayrs.apps.shosetsu.databinding.ControllerNovelInfoHeaderBinding
-import com.google.android.material.chip.Chip
 
 /*
  * This file is part of shosetsu.
@@ -63,16 +53,8 @@ data class NovelUI(
 	var tags: List<String>,
 
 	var status: Novel.Status,
-) : BaseRecyclerItem<ViewHolder>(), Convertible<NovelEntity> {
+) : Convertible<NovelEntity> {
 
-	override val layoutRes: Int = R.layout.controller_novel_info_header
-	override val type: Int = R.layout.controller_novel_info_header
-
-	/**
-	 * Identifier made negative to avoid conflicts with [ChapterUI]
-	 */
-	override var identifier: Long = -1091
-	override var isSelectable: Boolean = false
 	override fun convertTo(): NovelEntity = NovelEntity(
 		id = id,
 		url = novelURL,
@@ -89,79 +71,4 @@ data class NovelUI(
 		tags = tags,
 		status = status
 	)
-
-	override fun getViewHolder(v: View): ViewHolder = ViewHolder(v)
-	class ViewHolder(view: View) : BindViewHolder<NovelUI, ControllerNovelInfoHeaderBinding>(view) {
-		override val binding: ControllerNovelInfoHeaderBinding by lazy {
-			ControllerNovelInfoHeaderBinding.bind(view)
-		}
-
-		override fun ControllerNovelInfoHeaderBinding.bindView(item: NovelUI, payloads: List<Any>) {
-			// Handle title
-			novelTitle.text = item.title
-			novelSite.text = item.extName
-			// Handle authors
-			if (item.authors.isNotEmpty())
-				novelAuthor.text = item.authors.takeIf {
-					it.isNotEmpty()
-				}?.joinToString(", ") ?: itemView.getString(R.string.none)
-
-			// Handle description
-			novelDescription.text = item.description
-
-			// Handle artists
-			if (item.artists.isNotEmpty())
-				novelArtists.text = item.artists.takeIf {
-					it.isNotEmpty()
-				}?.joinToString(", ") ?: itemView.getString(R.string.none)
-
-			// Handles the status of the novel
-			when (item.status) {
-				Novel.Status.PAUSED -> novelPublish.setText(R.string.paused)
-				Novel.Status.COMPLETED -> novelPublish.setText(R.string.completed)
-				Novel.Status.PUBLISHING -> novelPublish.setText(R.string.publishing)
-				else -> novelPublish.setText(R.string.unknown)
-			}
-
-			// Inserts the chips for genres
-			novelGenres.removeAllViews()
-			for (string in item.genres) {
-				val chip = Chip(novelGenres.context)
-				chip.text = string
-				novelGenres.addView(chip)
-			}
-
-			// Loads the image
-			listOf(novelImage, novelImageBackground).forEach { iV: ImageView? ->
-				if (item.imageURL.isNotEmpty()) {
-					iV?.shosetsuLoad(item.imageURL)
-				} else {
-					iV?.setImageResource(R.drawable.broken_image)
-				}
-			}
-
-			if (item.bookmarked) {
-				inLibrary.setChipIconResource(R.drawable.ic_heart_svg_filled)
-				inLibrary.setText(R.string.controller_novel_in_library)
-			} else {
-				inLibrary.setChipIconResource(R.drawable.ic_heart_svg)
-				inLibrary.setText(R.string.controller_novel_add_to_library)
-			}
-		}
-
-		override fun ControllerNovelInfoHeaderBinding.unbindView(item: NovelUI) {
-			inLibrary.setChipIconResource(R.drawable.ic_heart_svg)
-			inLibrary.setText(R.string.controller_novel_add_to_library)
-			listOf(novelImage, novelImageBackground).forEach { iV: ImageView? ->
-				iV?.setImageResource(R.drawable.broken_image)
-			}
-			novelGenres.removeAllViews()
-			novelPublish.setText(R.string.unknown)
-			novelArtists.text = null
-			novelDescription.text = null
-			novelAuthor.text = null
-			novelSite.text = null
-			novelTitle.text = null
-		}
-	}
 }
