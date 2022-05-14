c62c89b4f3628f813e6a419fec22fc6a0a616635
doomsdayrs <doomsdayrs@gmail.com>
2022-05-14 13:32:14 -0400

    CatalogController.kt up

---
+++ android/src/main/java/app/shosetsu/android/view/compose/LazyGridScope.kt
@@ -0,0 +1,91 @@
+package app.shosetsu.android.view.compose
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.runtime.Composable
+import androidx.paging.PagingData
+import androidx.paging.PagingDataDiffer
+import androidx.paging.compose.LazyPagingItems
+import kotlinx.coroutines.flow.Flow
+
+/*
+ * This file is part of shosetsu.
+ *
+ * shosetsu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shosetsu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with shosetsu.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @since 14 / 05 / 2022
+ * @author Doomsdayrs
+ */
+
+/**
+ * Adds the [LazyPagingItems] and their content to the scope where the content of an item is
+ * aware of its local index. The range from 0 (inclusive) to [LazyPagingItems.itemCount] (exclusive)
+ * always represents the full range of presentable items, because every event from
+ * [PagingDataDiffer] will trigger a recomposition.
+ *
+ * @sample androidx.paging.compose.samples.ItemsIndexedDemo
+ *
+ * @param items the items received from a [Flow] of [PagingData].
+ * @param key a factory of stable and unique keys representing the item. Using the same key
+ * for multiple items in the list is not allowed. Type of the key should be saveable
+ * via Bundle on Android. If null is passed the position in the list will represent the key.
+ * When you specify the key the scroll position will be maintained based on the key, which
+ * means if you add/remove items before the current visible item the item with the given key
+ * will be kept as the first visible one.
+ * @param itemContent the content displayed by a single item. In case the item is `null`, the
+ * [itemContent] method should handle the logic of displaying a placeholder instead of the main
+ * content displayed by an item which is not `null`.
+ */
+fun <T : Any> LazyGridScope.itemsIndexed(
+	items: LazyPagingItems<T>,
+	key: ((index: Int, item: T) -> Any)? = null,
+	itemContent: @Composable LazyGridScope.(index: Int, value: T?) -> Unit
+) {
+	items(
+		count = items.itemCount,
+		key = if (key == null) null else { index ->
+			val item = items.peek(index)
+			if (item == null) {
+				PagingPlaceholderKey(index)
+			} else {
+				key(index, item)
+			}
+		}
+	) { index ->
+		itemContent(index, items[index])
+	}
+}
+
+data class PagingPlaceholderKey(private val index: Int) : Parcelable {
+	override fun writeToParcel(parcel: Parcel, flags: Int) {
+		parcel.writeInt(index)
+	}
+
+	override fun describeContents(): Int {
+		return 0
+	}
+
+	companion object {
+		@Suppress("unused")
+		@JvmField
+		val CREATOR: Parcelable.Creator<PagingPlaceholderKey> =
+			object : Parcelable.Creator<PagingPlaceholderKey> {
+				override fun createFromParcel(parcel: Parcel) =
+					PagingPlaceholderKey(parcel.readInt())
+
+				override fun newArray(size: Int) = arrayOfNulls<PagingPlaceholderKey?>(size)
+			}
+	}
+}
---
+++ android/src/main/java/app/shosetsu/android/view/compose/NovelCard.kt
@@ -0,0 +1,107 @@
+package app.shosetsu.android.view.compose
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.github.doomsdayrs.apps.shosetsu.R
+
+/*
+ * This file is part of shosetsu.
+ *
+ * shosetsu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shosetsu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with shosetsu.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Shosetsu
+ *
+ * @since 14 / 05 / 2022
+ * @author Doomsdayrs
+ */
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun NovelCardNormalContent(
+	title: String,
+	imageURL: String,
+	onClick: () -> Unit,
+	onLongClick: () -> Unit,
+	overlay: (BoxScope.() -> Unit)? = null,
+) {
+	Card(
+		modifier = Modifier.combinedClickable(
+			onClick = onClick,
+			onLongClick = onLongClick
+		).padding(4.dp)
+	) {
+		Box {
+			AsyncImage(
+				ImageRequest.Builder(LocalContext.current)
+					.data(imageURL)
+					.placeholder(R.drawable.animated_refresh)
+					.error(R.drawable.broken_image)
+					.build(),
+				stringResource(R.string.controller_novel_info_image),
+				modifier = Modifier
+					.fillMaxSize()
+					.aspectRatio(.75f)
+					.clickable(onClick = onClick)
+			)
+
+			Box(
+				modifier = Modifier.aspectRatio(.75f)
+					.fillMaxSize().drawWithCache {
+						onDrawWithContent {
+
+							drawRect(
+								brush = Brush.linearGradient(
+									listOf(
+										Color.Transparent,
+										Color.Black.copy(alpha = .75f),
+									),
+									Offset(0.0f, 0.0f),
+									Offset(0.0f, Float.POSITIVE_INFINITY),
+									TileMode.Clamp
+								)
+							)
+						}
+					}
+			)
+			Text(
+				title,
+				modifier = Modifier.align(Alignment.BottomCenter),
+				textAlign = TextAlign.Center,
+				color = Color.White
+			)
+			if (overlay != null)
+				overlay()
+		}
+	}
+}
