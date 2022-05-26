c67058500582e777506f8caa3f5c829b5e38e1b4
doomsdayrs <doomsdayrs@gmail.com>
2022-05-25 20:01:18 -0400

    Fix chapter scroll via resume going to the wrong chapter
    Closes #213

--- android/src/main/java/app/shosetsu/android/viewmodel/impl/NovelViewModel.kt
+++ android/src/main/java/app/shosetsu/android/viewmodel/impl/NovelViewModel.kt
@@ -355,17 +355,17 @@ class NovelViewModel(
 			val sortedArray = array.sortedBy { it.order }
 			val result = isChaptersResumeFirstUnread()
 
-			val index: Int = if (!result)
-				sortedArray.indexOfFirst { it.readingStatus != ReadingStatus.READ }
-			else sortedArray.indexOfFirst { it.readingStatus == ReadingStatus.UNREAD }
+			val item = if (!result)
+				sortedArray.firstOrNull { it.readingStatus != ReadingStatus.READ }
+			else sortedArray.firstOrNull { it.readingStatus == ReadingStatus.UNREAD }
 
 
 			emit(
-				if (index == -1) {
+				if (item == null) {
 					null
 				} else {
-					itemIndex.emit(index + 1) // +1 to account for header
-					sortedArray[index]
+					itemIndex.emit(array.indexOf(item) + 1) // +1 to account for header
+					item
 				}
 			)
 		}.onIO()
