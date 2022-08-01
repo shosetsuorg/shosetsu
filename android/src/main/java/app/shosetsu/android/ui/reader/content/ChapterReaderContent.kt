f706fc5c839b0ecc58c0cf3333b72108871e00d6
doomsdayrs <doomsdayrs@gmail.com>
2022-07-31 23:12:21 -0400

    Force reader bottom bar to be sharp

--- android/src/main/java/app/shosetsu/android/ui/reader/content/ChapterReaderContent.kt
+++ android/src/main/java/app/shosetsu/android/ui/reader/content/ChapterReaderContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -119,6 +120,7 @@ fun ChapterReaderContent(
 		sheetPeekHeight = if (!isFocused) BottomSheetScaffoldDefaults.SheetPeekHeight else 0.dp,
 		content = { paddingValues ->
 			content(paddingValues)
-		}
+		},
+		sheetShape = RectangleShape
 	)
 }
