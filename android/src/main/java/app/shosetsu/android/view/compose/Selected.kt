6f5927913cfb032ffdef82d816a8326632df62c4
Jobobby04 <jobobby04@users.noreply.github.com>
2022-08-07 21:27:38 -0400

    Licenses

--- android/src/main/java/app/shosetsu/android/view/compose/Selected.kt
+++ android/src/main/java/app/shosetsu/android/view/compose/Selected.kt
@@ -9,6 +9,23 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.unit.dp
 
+/*
+ * This file is part of shosetsu.
+ *
+ * shosetsu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shosetsu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with shosetsu.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 fun Modifier.selectedOutline(isSelected: Boolean) = composed {
     val secondary = MaterialTheme.colors.secondary
     if (isSelected) {
